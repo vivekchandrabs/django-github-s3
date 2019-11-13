343cf5c5fd6cfe93e717bb63e669f4786a720adc
Vivek Chandra B S <vivek.chandra@speckbit.com>
2019-11-13 10:34:10 +0530

    Delete models and its migrations

--- github_storages/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models	
-
-class ImageInfo(models.Model):
-	image_url = models.CharField(max_length=500)
-	sha = models.CharField(max_length=200)
-
-	def __str__(self):
-		return f"{self.image_url} | {self.sha}"
