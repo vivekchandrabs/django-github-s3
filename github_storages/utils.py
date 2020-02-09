e0333e897a289eac228dadd10bbcecd3148d0826
Vivek Chandra B S <vivek.chandra@speckbit.com>
2020-02-09 22:34:21 +0530

    Add code to construct url for bucket

--- github_storages/utils.py
+++ github_storages/utils.py
@@ -1,14 +1,24 @@
 from django.conf import settings
 
-def get_url(name):
+def get_url(name, media_bucket):
 	'''
 	Helper function to construct the url of the file.
 	'''
-	url = "https://api.github.com/repos/"
-	url += str(settings.GITHUB_HANDLE) + "/"
-	url += str(settings.GITHUB_REPO_NAME) 
-	url += "/contents/"
-	url += str(name)
+	if media_bucket == None:
+		url = "https://api.github.com/repos/"
+		url += str(settings.GITHUB_HANDLE) + "/"
+		url += str(settings.GITHUB_REPO_NAME) 
+		url += "/contents/"
+		url += str(name)
 
-	return url
+		return url
 
+	else:
+		url = "https://api.github.com/repos/"
+		url += str(settings.GITHUB_HANDLE) + "/"
+		url += str(settings.GITHUB_REPO_NAME) 
+		url += "/contents/"
+		url += str(media_bucket) + "/"
+		url += str(name)
+
+		return url
