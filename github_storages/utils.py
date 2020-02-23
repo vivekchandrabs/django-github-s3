453f4b965b2cfac6bd1ae9609ee3eb9835bfbb2b
Vivek Chandra B S <vivek.chandra@speckbit.com>
2020-02-24 01:03:51 +0530

    Modify get_url method to return the total path of the file

--- github_storages/utils.py
+++ github_storages/utils.py
@@ -1,24 +1,23 @@
 from django.conf import settings
 
-def get_url(name, media_bucket):
+def get_url(name, path, media_bucket):
 	'''
 	Helper function to construct the url of the file.
 	'''
+	url = "https://api.github.com/repos/"
+	url += str(settings.GITHUB_HANDLE) + "/"
+	url += str(settings.GITHUB_REPO_NAME) 
+	url += "/contents/"
+
 	if media_bucket == None:
-		url = "https://api.github.com/repos/"
-		url += str(settings.GITHUB_HANDLE) + "/"
-		url += str(settings.GITHUB_REPO_NAME) 
-		url += "/contents/"
 		url += str(name)
-
-		return url
-
 	else:
-		url = "https://api.github.com/repos/"
-		url += str(settings.GITHUB_HANDLE) + "/"
-		url += str(settings.GITHUB_REPO_NAME) 
-		url += "/contents/"
 		url += str(media_bucket) + "/"
-		url += str(name)
 
-		return url
+	if len(path) != 0:
+		for folder_name in path:
+			url += str(folder_name) + "/"
+
+	url += str(name)
+
+	return url
